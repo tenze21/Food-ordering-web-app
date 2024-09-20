2f7e2439704ec33b76acedbd4a6f44a29a864305
Sonam Wangmo <12230047.gcit@rub.edu.bt>
2024-09-20 23:39:05 +0600

    registration

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express= require("express");
 const morgan= require("morgan");
+const userRouter = require('./routes/userRoutes')
 
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use('/api/v1/users', userRouter)
 
 module.exports= app;
 
---
+++ controllers/userController.js
@@ -0,0 +1,42 @@
+const User = require('./../models/userModels')
+exports.getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find()
+        res.status(200).json({data: users, status: 'success'})
+    } catch (err) {
+        res.status(500).json({ error: err.message});
+    }
+}
+exports.createUser = async(req, res) => {
+    try {
+        const user = await User.create(req.body);
+        console.log(req.body.name)
+        res.json({ data: user, status: "success" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+exports.getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.json({ data: user, status: "success" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+exports.updateUser = async(req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body);
+        res.json({ data: user, status: "success"});
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+exports.deleteUser = async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id, req.body);
+        res.json({ data: user, status: "success"});
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
